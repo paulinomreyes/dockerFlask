3f714cb435606b288f5fb4548542b35d732efc12
paulinomreyes <paulinomreyes@gmail.com>
2017-11-12 16:54:43 +0000

    Update Flask Application

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import Flask, jsonify, request, session, render_template, abort, session, url_for, escape, redirect
+import os
+import sys
+import re
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return render_template('index.html')
+
